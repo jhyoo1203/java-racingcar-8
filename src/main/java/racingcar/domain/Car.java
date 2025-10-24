d4bdf3af1e0bf961bc9c47b87211da7b06512531
JaeHyeon Yoo <jhyoo1203@gmail.com>
2025-10-24 17:05:12 +0900

    feat: 자동차 도메인 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,28 @@
+package racingcar.domain;
+
+public class Car {
+
+    private final String name;
+    private int position;
+
+    private Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public static Car from(String name) {
+        return new Car(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void moveForward() {
+        position++;
+    }
+}
