4729364d5319ee128d78693bbf3880a27792e163
JaeHyeon Yoo <jhyoo1203@gmail.com>
2025-10-24 17:08:05 +0900

    feat: 레이싱 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.config.AppConfig;
+import racingcar.presentation.RacingController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig config = new AppConfig();
+        RacingController racingController = config.getRacingController();
+
+        racingController.start();
     }
 }
