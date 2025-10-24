aebb3e96f1894e691aad4e4abad757ca0cd0538b
JaeHyeon Yoo <jhyoo1203@gmail.com>
2025-10-24 17:09:04 +0900

    fix: 순서 보장 못하는 오류 수정

--- src/main/java/racingcar/domain/repository/CarRepository.java
+++ src/main/java/racingcar/domain/repository/CarRepository.java
@@ -2,13 +2,13 @@ package racingcar.domain.repository;
 
 import racingcar.domain.Car;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class CarRepository {
 
-    private static final Map<String, Car> carMap = new HashMap<>();
+    private static final Map<String, Car> carMap = new LinkedHashMap<>();
 
     public void saveAll(List<Car> cars) {
         for (Car car : cars) {
